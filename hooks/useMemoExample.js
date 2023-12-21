53b1809f2cc61a162883ded07edc6fad4d4f5960
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2023-12-21 19:01:08 +0530

    Update useMemoExample.js

--- hooks/useMemoExample.js
+++ hooks/useMemoExample.js
@@ -1,32 +1,26 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useState } from "react";
 
-export const initialItems = new Array(29_999_999).fill(0).map((_, i) => {
-  return {
-    id: i,
-    isSelected: i === 29_999_998,
-  };
-});
+const initialItems = new Array(29_999_999).fill(0).map((_, i) => ({
+  id: i,
+  isSelected: i === 29_999_998,
+}));
 
-function Demo() {
+function UseMemoExample() {
   const [count, setCount] = useState(0);
   const [items, setItems] = useState(initialItems);
 
-  // this expensive computation only runs if the items array change. 
+  // this expensive computation only runs if the items array change.
   // this will not run for every render.
   const selectedItem = useMemo(
     () => items.find((item) => item.isSelected),
-    [items],
+    [items]
   );
 
   return (
-    <div className='tutorial'>
+    <div>
       <h1>count: {count}</h1>
       <h1>selected item: {selectedItem?.id}</h1>
-      <button onClick={() => setCount(count + 1)}>
-        increment
-      </button>
+      <button onClick={() => setCount(count + 1)}>increment</button>
     </div>
   );
 }
-
-export default Demo;
