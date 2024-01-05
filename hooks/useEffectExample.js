1a926395ab5d7fe65c01056497a13804c214f44b
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-01-05 19:19:26 +0530

    Create useEffectExample.js

---
+++ hooks/useEffectExample.js
@@ -0,0 +1,26 @@
+// You can use useEffect to perform effects like fetching data, using browser APIs etc
+
+import { useEffect } from 'react';
+
+function Example() {
+  const [data, setData] = useState("");
+
+  // useEffect takes a function and a dependency array as arguments
+  useEffect(()=>{
+    async function fetchData (){
+      const response = await fetch(URL)
+      const data = await response.json()
+      setData(data)
+    }
+    fetchData()
+
+    // optionally we can provide clean-up function in the return statement
+  }, [])
+
+  return (
+    <div>
+      <p>Fetched Data - {data}</p>
+    </div>
+  );
+}
+
