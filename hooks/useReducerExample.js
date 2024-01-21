963e03f65d0bd4e963eeaf00d9a4b0c75e1dcd30
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-01-21 22:40:44 +0530

    Create useReducerExample.js

---
+++ hooks/useReducerExample.js
@@ -0,0 +1,24 @@
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
+    </div>
+  );
+}
